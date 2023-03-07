ef20fc6bd03b4e923dd50cc2c476f72c50105b1f
Sara Martinez <sara.martinez.hidalgo@gmail.com>
2023-03-07 10:47:53 -0300

    add first main page prototype

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { buscar, ordenar, filtrar } from "./dataHandler.js"
 
-console.log(example, data);
+//Elementos DOM en variable
+const btnBuscar = document.getElementById("btnBuscar")
+const bookTitle = document.getElementById("bookTitle")
+const bookReleaseDay = document.getElementById("bookReleaseDay")
+const bookAuthor = document.getElementById("bookAuthor")
+const bookDescription = document.getElementById("bookDescription")
+// const btnOrdenar = document.getElementsById("ordenar")
+// const books = document.getElementsByName("books")
+
+//Eventos del DOM
+btnBuscar.addEventListener('click', buscarInfo)
+// btnFiltrar.addEventListener('click',)
+// btnOrdenar.addEventListener('click',)
+
+//Funciones que interactuan con el DOM
+function buscarInfo() {
+  const resultado = buscar()
+  bookTitle.innerText = resultado[0].title
+  bookReleaseDay.innerText = resultado[0].releaseDay
+  bookAuthor.innerText = resultado[0].author
+  bookDescription.innerText = resultado[0].description
+}
